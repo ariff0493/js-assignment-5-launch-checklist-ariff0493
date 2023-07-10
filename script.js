66a3a2727d7c506610457dcb1aa7a22772b255cd
ariff0493 <ariff0493@gmail.com>
2023-07-10 15:53:22 -0500

    finished

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+    // In order to create a event.preventDefault I have created a form variable to select the form element in the HTML file.
+    // I have then used the addEventListener method to listen for the submit event. When the submit event is triggered the event.preventDefault method will prevent the page from reloading.
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
 
+        let pilotNameInput = document.querySelector("input[name=pilotName]");
+        let copilotNameInput = document.querySelector("input[name=copilotName]");
+        let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+        let cargoMassInput = document.querySelector("input[name=cargoMass]");
+        let faultyItems = document.getElementById("faultyItems");
+        
+        //I am then calling the formSubmission function with the appropriate arguments to display the appropriate values in the HTML file.
+        formSubmission(document, faultyItems, pilotNameInput.value, copilotNameInput.value, fuelLevelInput.value, cargoMassInput.value);
+    });
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+    // Here I am creating variables with dot notation to select the missionTarget div in the HTML file to be used in the pickPlanet function.
+        let planet = pickPlanet(listedPlanets);
+        let name = planet.name;
+        let diameter = planet.diameter;
+        let star = planet.star;
+        let distance = planet.distance;
+        let moons = planet.moons;
+        let imageUrl = planet.image;
+        //Then calling the addDestinationInfo function to add the planet information to the missionTarget div.
+        addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
+    })
    
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -3,6 +3,8 @@ require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
+   // I have created a missionTarget variable to help me select the missionTarget div in the HTML file.
+   // I am then using the innerHTML property to add the HTML formatting to the missionTarget div.
    let missionTarget = document.getElementById("missionTarget");
    missionTarget.innerHTML = `
                 <h2>Mission Destination</h2>
@@ -18,6 +20,8 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
+
+   // Here I am creating an if else statement to check if the input is empty, a number, or a string. This validation then returns a string to be used in the formSubmission function.
    if (testInput === "" || testInput === null || testInput === undefined) {
       return "Empty";
    } else if (isNaN(testInput)) {
@@ -28,10 +32,15 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+
+   // In the formSubmission function I have defined the pilot, copilot, fuel, and cargo status variables. I have also created the list variable to be used in the if else statements below.
+   // Using the if else statements and the validateInput function I am returning a message to the user based on the input they have entered.
+   // If a number is put inside of the pilot or copilot input fields, the user will be alerted to enter a valid input. Same if a string is entered into the fuel level or cargo mass
    let pilotStatus = document.getElementById("pilotStatus");
    let copilotStatus = document.getElementById("copilotStatus");
    let fuelStatus = document.getElementById("fuelStatus");
    let cargoStatus = document.getElementById("cargoStatus");
+   list = document.getElementById("faultyItems");
 
    if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
       alert("All fields are required!");
@@ -41,10 +50,11 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
       pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
       copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
    }
-
+// Below I have also created another if else statement to test if the fuel level and cargo level are at appropriate levels to launch the shuttle.
+// If the values entered are too high or low a message will return inside of the list variable and the launch status will change to red.
    if ((fuelLevel) < 10000) {
       fuelStatus.innerHTML = "Fuel level too low for launch";
-      faultyItems.style.visibility = "visible";
+      list.style.visibility = "visible";
       launchStatus.innerHTML = "Shuttle not ready for launch";
       launchStatus.style.color = "red";
    } else if ((cargoLevel) > 10000) {
@@ -58,7 +68,7 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
    }
 
    }
-
+// Here I am just calling the myFetch function to be used in the script.js file.
 async function myFetch() {
     let planetsReturned;
 
@@ -68,7 +78,8 @@ async function myFetch() {
 
     return planetsReturned;
 }
-
+// I was kind of given teh instructions for this one really, but I am just creating a function to pick a random planet from the array of planets.
+// With Math.floor I am being given an even number, and using Math.random multiplied by the length of the array I am getting a random number between 0 and 5.
 function pickPlanet(planets) {
    let index = Math.floor(Math.random() * planets.length);
    return planets[index];
